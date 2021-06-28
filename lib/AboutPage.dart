a4c590eb877e8480bb96d480abe591ba5b8b19f2
Sean Wen <sw92410@hotmail.com>
2021-06-28 15:34:11 -0600

    - Add about content

--- lib/AboutPage.dart
+++ lib/AboutPage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
   @override
@@ -8,7 +10,39 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('About'),
       ),
-      body: Center(child: Text('About'))
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: 
+                  "This is app is powered by Jisho.org and uses it's public api to access Japanese vocabularies in different categories.\n"
+                  "To see the source code ",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  height: 1.5
+                )
+              ),
+              TextSpan(
+                text: "Click here",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 15,
+                  height: 1.5
+                ),
+                recognizer: TapGestureRecognizer()..onTap = () async {
+                  String url = "https://github.com/ShouShou92410/jpn_quiz";
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  }
+                }
+              )
+            ]
+          ),
+        )
+      )
     );
   }
 }
